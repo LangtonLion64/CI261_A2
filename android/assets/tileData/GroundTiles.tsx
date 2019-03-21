d711f0224be0898295ad6d220d9adacaac4f9b08
bd242 <b.davies10@uni.brighton.ac.uk>
2019-03-21 17:02:26 +0000

    Temporary map

---
+++ android/assets/tileData/GroundTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="2019.01.30" name="GroundTiles" tilewidth="64" tileheight="64" tilecount="16" columns="4">
+ <image source="Tiles/Tile/GroundTiles.png" width="256" height="256"/>
+</tileset>
