0abb829c038a1c6eca4cf7d908f7852e70c89bdc
xyband99 <bdavies125@gmail.com>
2019-05-13 20:24:05 +0100

    Removing old sprites and add new ones with new map. (bd242)

---
+++ android/assets/tileData/Saloon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="Saloon" tilewidth="256" tileheight="196" tilecount="1" columns="1">
+ <image source="Tiles/Saloon.png" width="256" height="196"/>
+</tileset>
